83f830ecb3aa6d09344b8afd9f6f21388872f71b
Your Name <you@example.com>
2023-05-18 00:29:43 +0600

    banner section added

--- src/shared/NavBar.jsx
+++ src/shared/NavBar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NavBar = () => {
     return (
-        <div className="navbar bg-base-100">
+        <div className="navbar bg-cyan-50">
             <div className="navbar-start">
                 <div className="dropdown">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
